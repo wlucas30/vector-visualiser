b6b87affb07d91ad7c0ee682e68e8cec4d40b53a
wlucas30 <115418741+wlucas30@users.noreply.github.com>
2024-06-18 23:35:12 +0100

    Currently selected vector tooltip

--- Vector Visualiser/Vector Visualiser/Planes/2DPlaneVisualiser.swift
+++ Vector Visualiser/Vector Visualiser/Planes/2DPlaneVisualiser.swift
@@ -7,25 +7,61 @@
 
 import SwiftUI
 
+struct Vector {
+    var id: UUID
+    var i: CGFloat
+    var j: CGFloat
+    var name: String?
+    var isUnitVector: Bool
+}
+
 struct _2DPlaneVisualiser: View {
     @State private var selectedVectorID: UUID? = nil
     @State private var secondarySelectedVectorID: UUID? = nil
     @Binding var currentDimension: Int?
     
+    @State private var vectors: [Vector] = [
+        Vector(id: UUID(), i: 2, j: 3, isUnitVector: false),
+        Vector(id: UUID(), i: 1, j: -4, isUnitVector: false),
+        Vector(id: UUID(), i: 1, j: 0, name: "i", isUnitVector: true),
+        Vector(id: UUID(), i: 0, j: 1, name: "j", isUnitVector: true)
+    ]
+    
     var body: some View {
+        if let _ = selectedVectorID {
+            let selectedVectorComponents = getComponentsOfVector(id: selectedVectorID!)
+            if let x = selectedVectorComponents {
+                Text("Currently selected vector: (\(x[0].formattedString()!), \(x[1].formattedString()!))")
+            } else {
+                Text("No vector is selected")
+            }
+        } else {
+            Text("No vector is selected")
+        }
+        
         VStack {
-            _2DCoordinateGrid(selectedVectorID: $selectedVectorID, secondarySelectedVectorID: $secondarySelectedVectorID)
+            _2DCoordinateGrid(selectedVectorID: $selectedVectorID, secondarySelectedVectorID: $secondarySelectedVectorID, vectors: $vectors)
                 .border(Color.black)
         }
         .onAppear() {
             currentDimension = 2
         }
     }
+    
+    private func getComponentsOfVector(id: UUID) -> [CGFloat]? {
+        for vector in vectors {
+            if vector.id == id {
+                return [vector.i, vector.j]
+            }
+        }
+        return nil
+    }
 }
 
 struct _2DCoordinateGrid: View {
     @Binding var selectedVectorID: UUID?
     @Binding var secondarySelectedVectorID: UUID?
+    @Binding var vectors: [Vector]
     
     var body: some View {
         GeometryReader { metrics in
@@ -41,7 +77,7 @@ struct _2DCoordinateGrid: View {
                     path.move(to: CGPoint(x: 0, y: yPos))
                     path.addLine(to: CGPoint(x: width, y: yPos))
                 }
-                .stroke(y == 0 ? Color.black : Color.gray, lineWidth: y == 0 ? 2 : 1)
+                .stroke(y == 0 ? Color(red: 0.3, green: 0.3, blue: 0.3) : Color.gray, lineWidth: y == 0 ? 1.5 : 1)
             }
             
             // Draw vertical grid lines
@@ -51,11 +87,13 @@ struct _2DCoordinateGrid: View {
                     path.move(to: CGPoint(x: xPos, y: 0))
                     path.addLine(to: CGPoint(x: xPos, y: height))
                 }
-                .stroke(x == 0 ? Color.black : Color.gray, lineWidth: x == 0 ? 2 : 1)
+                .stroke(x == 0 ? Color(red: 0.3, green: 0.3, blue: 0.3): Color.gray, lineWidth: x == 0 ? 1.5 : 1)
             }
             
-            _2DVectorArrow(i: 2, j: 3, selectedVectorID: $selectedVectorID, secondarySelectedVectorID: $secondarySelectedVectorID)
-            _2DVectorArrow(i: 1, j: -4, selectedVectorID: $selectedVectorID, secondarySelectedVectorID: $secondarySelectedVectorID)
+            ForEach(vectors, id: \.id) { vector in
+                _2DVectorArrow(i: vector.i, j: vector.j, id: vector.id, name: vector.name, isUnitVector: vector.isUnitVector, selectedVectorID: $selectedVectorID, secondarySelectedVectorID: $secondarySelectedVectorID)
+            }
+            
         }
         .frame(width: 450, height: 450) // Set frame size
     }
@@ -64,7 +102,9 @@ struct _2DCoordinateGrid: View {
 struct _2DVectorArrow: View {
     var i: CGFloat
     var j: CGFloat
-    var id: UUID? = UUID()
+    var id: UUID
+    var name: String?
+    var isUnitVector: Bool
     
     @State private var isCommandKeyPressed: Bool = false
     
@@ -99,29 +139,43 @@ struct _2DVectorArrow: View {
                 y: endY - arrowLength * sin(angle - arrowAngle)
             )
             
-            Path { path in
-                // Draw the vector line
-                path.move(to: CGPoint(x: startX, y: startY))
-                path.addLine(to: CGPoint(x: endX, y: endY))
+            ZStack {
                 
-                // Draw the arrowhead
-                path.move(to: CGPoint(x: endX, y: endY))
-                path.addLine(to: arrowPoint1)
-                path.move(to: CGPoint(x: endX, y: endY))
-                path.addLine(to: arrowPoint2)
-            }
-            .stroke(selectedVectorID == id ? Color.blue : (secondarySelectedVectorID == id ? Color.red : Color.black), lineWidth: 2)
-            .onTapGesture {
-                if (isCommandKeyPressed) {
-                    if let x = selectedVectorID { // ensure a primary vector is selected
-                        if x != id {
-                            secondarySelectedVectorID = id // ensures a vector cannot be selected twice
+                Path { path in
+                    // Draw the vector line
+                    path.move(to: CGPoint(x: startX, y: startY))
+                    path.addLine(to: CGPoint(x: endX, y: endY))
+                    
+                    // Draw the arrowhead
+                    path.move(to: CGPoint(x: endX, y: endY))
+                    path.addLine(to: arrowPoint1)
+                    path.move(to: CGPoint(x: endX, y: endY))
+                    path.addLine(to: arrowPoint2)
+                }
+                .stroke(selectedVectorID == id ? Color.blue : (secondarySelectedVectorID == id ? Color.red : Color.black), lineWidth: 2.3)
+                .onTapGesture {
+                    if !(isUnitVector) {
+                        if (isCommandKeyPressed) {
+                            if let x = selectedVectorID { // ensure a primary vector is selected
+                                if x != id {
+                                    secondarySelectedVectorID = id // ensures a vector cannot be selected twice
+                                }
+                            }
+                        } else {
+                            secondarySelectedVectorID = nil
+                            selectedVectorID = id
                         }
                     }
-                } else {
-                    secondarySelectedVectorID = nil
-                    selectedVectorID = id
                 }
+                
+                if isUnitVector, let name = name {
+                    Text(name)
+                        .font(.subheadline)
+                        .foregroundStyle(.blue)
+                        .fontWeight(.heavy)
+                        .position(x: endX + 7, y: endY - 7)
+                }
+                
             }
         }
         .frame(width: 450, height: 450) // Set frame size
@@ -146,5 +200,17 @@ struct KeyPressModifier: ViewModifier {
 extension View {
     func trackKeyPress(isCommandKeyPressed: Binding<Bool>) -> some View {
         self.modifier(KeyPressModifier(isCommandKeyPressed: isCommandKeyPressed))
+    }
+}
+
+extension CGFloat {
+    func formattedString() -> String? {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.maximumFractionDigits = 2
+        numberFormatter.minimumFractionDigits = 0
+        numberFormatter.locale = Locale.current
+        
+        return numberFormatter.string(from: NSNumber(value: Double(self)))
     }
 }
